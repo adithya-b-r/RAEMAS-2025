82132c6cbb9ffc208fc6f4a7a017f2a3bc634796
Abr009 <bradithya945@gmail.com>
2025-05-10 13:47:58 +0530

    Fixed Scrolling Issues.

--- src/Components/Navbar.jsx
+++ src/Components/Navbar.jsx
@@ -12,7 +12,7 @@ export const Navbar = () => {
   };
 
   return (
-    <nav className="w-full md:h-[10vh] h-[9vh] bg-purple-700/90 backdrop-blur-lg shadow-lg flex justify-between items-center pl-5 pr-8 fixed top-0 left-0 z-12">
+    <nav className="w-full md:h-[10vh] h-[9vh] bg-purple-700/95 backdrop-blur-lg shadow-lg flex justify-between items-center pl-5 pr-8 fixed top-0 left-0 z-12">
       <div className="text-white font-bold text-2xl tracking-wider">RAEMAS 2025</div>
       <div className="md:flex hidden text-white font-semibold gap-8">
         <a onClick={() => scrollToSection('home')} className="cursor-pointer hover:text-gray-300 transition-all">Home</a>
